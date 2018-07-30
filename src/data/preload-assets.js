73b161f73a416912d4d95d267b1226a49b44a15b
Iran Reyes <iran.reyes@jam3.com>
2018-07-30 18:45:42 -0400

    Remove preloading sample data

--- src/data/preload-assets.js
+++ src/data/preload-assets.js
@@ -11,11 +11,7 @@ const ASSETS_PATH = process.env.PUBLIC_URL + '/assets/';
  * @const {array} TABLET_ASSETS (Keep an assets list for tablet)
  * @const {array} DESKTOP_ASSETS (Keep an assets list for desktop)
  */
-const GLOBAL_ASSETS = [
-  `${ASSETS_PATH}button-rollover.mp3`,
-  `${ASSETS_PATH}button-click.mp3`,
-  `${ASSETS_PATH}button-sprite.mp3`
-];
+const GLOBAL_ASSETS = [];
 const MOBILE_ASSETS = [];
 const TABLET_ASSETS = [];
 const DESKTOP_ASSETS = [];
